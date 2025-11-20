9701746e71dd941ebff7da6f0e0d6e208cc1ff58
Mohamed Ahmed <50152939+Mohamed-Ahmed10@users.noreply.github.com>
2025-11-20 19:14:25 +0200

    Libraries

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import './App.css';
-import Sample from './components/Sample';
+import './App.scss';
+import About from './views/About';
+import Contact from './views/Contact';
+import Home from './views/Home';
 
 function App() {
   return (
-    <Sample />
+    <div className='text-center'>
+      Hello
+      <Home />
+      <About />
+      <Contact />
+    </div>
   )
 }
 
---
+++ src/views/Home.jsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+    return (
+        <div className="home">
+            <div className="child">This is child</div>
+        </div>
+    )
+}
